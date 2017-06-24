661bfcc7e322ebb130a7e2894fb9bbd6b9871171
Ashley Hindle <ashleyhindle@users.noreply.github.com>
2017-06-24 12:35:10 +0100

    Update provisioner.sh

--- fodor/provisioner.sh
+++ fodor/provisioner.sh
@@ -47,4 +47,5 @@ sed -i -e 's/password_secret =.*/password_secret = '$SECRET'/' /etc/graylog/serv
 PASSWORD=$(echo -n $ADMIN_PASSWORD | shasum -a 256 | awk '{print $1}')
 sed -i -e 's/root_password_sha2 =.*/root_password_sha2 = '$PASSWORD'/' /etc/graylog/server/server.conf
 
+/etc/init/graylog-server.override
 start graylog-server
